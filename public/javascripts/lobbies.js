fb311a5b627d1f0f00f15a3c891cd813c726d288
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-01-28 15:57:59 +0100

    joining games now works (js closures time!)

--- public/javascripts/lobbies.js
+++ public/javascripts/lobbies.js
@@ -1,9 +1,26 @@
-window.addEventListener('load', (event) => {
-    document.getElementById('newGame').addEventListener('click', (event) => {
+window.addEventListener('load', () => {
+    document.getElementById('newGame').addEventListener('click', () => {
        newGame();
     });
+    let id = 0;
+
+    // chrome's something something content policy disallows used of inline js, so we have to add event listeners
+    // manually when loading the page
+    while (document.getElementById('j' + id) !== null) {
+        let localJoin = makeJoin(id);
+        document.getElementById('j' + id).addEventListener('click', () => {
+            localJoin();
+        });
+        id++;
+    }
 });
 
 const newGame = () => {
-    window.location.replace('/new');
+    window.location = '/new';
+}
+
+const makeJoin = (id) => () => join(id);
+
+const join = (id) => {
+    window.location = `/game/${id}`;
 }
